4b4416edb9b36f3c3574b02d5f9c7cd3ad3d75cd
immotare <immortal3409fgh@gmail.com>
2022-03-04 15:46:35 +0900

    step7

--- net.h
+++ net.h
@@ -23,13 +23,19 @@
 #define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
 #define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "up" : "down")
 
-#define NET_PROTOCOL_TYPE_IP 0x0800
-#define NET_PROTOCOL_TYPE_ARP 0x0806
+#define NET_PROTOCOL_TYPE_IP   0x0800
+#define NET_PROTOCOL_TYPE_ARP  0x0806
 #define NTT_PROTOCOL_TYPE_IPV6 0x86dd
+
+#define NET_IFACE_FAMILY_IP   1
+#define NET_IFACE_FAMILY_IPV6 2
+
+#define NET_IFACE(x) ((struct net_iface *)(x))
 
 struct net_device {
     struct net_device *next;
     unsigned int index;
+    struct net_iface *ifaces; /* NOTE: if you want to add/delete the entries after net_run(), you need to protect ifaces with a mutex. */
     char name[IFNAMSIZ];
     uint16_t type;
     uint16_t mtu;
@@ -51,10 +57,21 @@ struct net_device_ops {
     int (*transmit)(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
 };
 
+struct net_iface {
+    struct net_iface *next;
+    struct net_device *dev; /* back pointer to parent */
+    int family;
+    /* depends on implementation of protocols. */
+};
+
 extern struct net_device *
 net_device_alloc(void);
 extern int
 net_device_register(struct net_device *dev);
+extern int
+net_device_add_iface(struct net_device *dev, struct net_iface *iface);
+extern struct net_iface *
+net_device_get_iface(struct net_device *dev, int family);
 extern int
 net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
 
